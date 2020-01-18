10148cfdb1a36253061b038c48f380aa0f59f71d
Baptiste-Caral <baptiste.caral@gmail.com>
2020-01-18 11:44:05 +0100

    description

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -4,10 +4,34 @@ import React from 'react';
 // == Import : local
 import './homepage.scss';
 
+// == import Composants
+import Banner from 'src/components/Banner';
+
 // == Composant
 const Homepage = () => (
   <div id="homepage">
-    Homepage
+    <Banner />
+    <div className="description">
+      <img className="description-logo" src="src/assets/img/logo_sans_fond.png" alt="" />
+      <p className="description-text">
+        Débloquez le meilleur d'oZone.
+      </p>
+      <p className="description-text">
+        Proposez vos Events sur l'écologie.
+      </p>
+      <p className="description-text">
+        Inscrivez vous aux events.
+      </p>
+      <p className="description-text">
+        Recevez des alertes de prix et des events pour agir sur la planete.
+      </p>
+      <p className="description-text">
+        Gagner des points blablabla lorem
+      </p>
+      <button type="button" className="description-button">
+      S'inscrire
+      </button>
+    </div>
   </div>
 );
 
