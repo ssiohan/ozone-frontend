1411ca229b82917775e696d77e0c15d8a9709008
Grace-Bilali <grace.bilali@gmail.com>
2020-01-18 10:07:19 +0100

    architecture of the page event

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -7,7 +7,17 @@ import './event.scss';
 // == Composant
 const Event = () => (
   <div id="event">
-    Page qui présente UN événement 
+    <div id="event_header">
+    Titre, label, nombre de participants, nom de l'organisateur, bouton modifier, bouton partager
+    </div>
+    <div id="event_description">
+      Image, description, bouton participer
+    </div>
+    <div id="event_footer">
+      Carte, lien Google maps et adresse
+      Infos sur l'event: participants, pénibilité, durée etc..
+      Date, numéro organisateur, nombre de points à gagner
+    </div>
   </div>
 );
 
