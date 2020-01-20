0f176208626adfc7c80c43b6218b0892ee716e5c
Grace-Bilali <grace.bilali@gmail.com>
2020-01-20 14:48:23 +0100

    addition of event author in the header

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -22,33 +22,49 @@ import EventAddress from './EventAddress';
 const Event = () => (
   <div id="event">
     {/** Header de la page avec titre,label,auteur,infos participants, bouton modifier & partage */}
-    <Grid
-      container
-      justify="space-between"
-    >
-      {/* Partie gauche du header */}
-      <Grid item container xs={12} md={9}>
-        <Grid item>
-          <h1 id="event_title">Ramassage de déchets sur la plage d'Armanville</h1>
+    <div id="event_header">
+      <Grid
+        container
+        justify="space-between"
+      >
+        {/* Partie gauche du header */}
+        <Grid item container xs={12} md={9}>
+          <Grid item>
+            <h1 id="event_title">Ramassage de déchets sur la plage d'Armanville</h1>
+          </Grid>
+          <Grid
+            item
+            container
+            spacing={5}
+          >
+            <Grid item>
+              <EventLabel />
+            </Grid>
+            <Grid item>
+              <p id="event_participants_progress">30/50 participants</p>
+              <ProgressBar />
+            </Grid>
+          </Grid>
+          <Grid item>
+            <p>
+              Proposé par : <span id="event_author_name"> Jean-Marc de la Compta</span>
+            </p>
+          </Grid>
         </Grid>
-        <Grid
-          item
-          container
-          spacing={5}
-        >
-          <Grid item>
-            <EventLabel />
-          </Grid>
-          <Grid item>
-            <p id="event_participants_progress">30/50 participants</p>
-            <ProgressBar />
-          </Grid>
+        {/* Partie droite header */}
+        <Grid item xs={12} md={3}>
+          <ModifyButton />
+          <ShareButton />
         </Grid>
       </Grid>
-      {/* Partie droite header */}
-      <Grid item xs={12} md={3}>
-        <ModifyButton />
-        <ShareButton />
+    </div>
+    {/** Partie centrale de la page avec photo, description et bouton participer */}
+    <Grid container>
+      <Grid item>
+        Partie gauche avec l'image
+      </Grid>
+      <Grid item>
+        Partie droite avec description et bouton participer
       </Grid>
     </Grid>
   </div>
