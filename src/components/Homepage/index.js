ea51c7e7a6c27d502018b7685bff57eb30d35e82
Baptiste-Caral <baptiste.caral@gmail.com>
2020-01-20 15:33:07 +0100

    composant cardweb

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,5 +1,9 @@
 // == Import : npm
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+
 
 // == Import : local
 import './homepage.scss';
@@ -7,23 +11,37 @@ import './homepage.scss';
 // == import Composants
 import Banner from 'src/components/Banner';
 import Description from './Description';
-import Card from './Card';
+import Cardweb from './Cardweb';
 
-// == Composant
-const Homepage = () => (
-  <div id="homepage">
-    <Banner />
-    <div className="container-homepage">
-      <Description />
-      <div className="card">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
-    </div>
-  </div>
-);
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
-// == Export
-export default Homepage;
+export default function FullWidthGrid() {
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.root} maxWidth="lg" id="app">
+      <Grid className={classes.homepage}>
+        <Banner />
+        <Grid className={classes.homepageContent} container spacing={3}>
+          <Grid item md={3}>
+            <Description />
+          </Grid>
+          <Grid item xs={12} sm={12} md={9}>
+            <Cardweb />
+            <Cardweb />
+            <Cardweb />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
