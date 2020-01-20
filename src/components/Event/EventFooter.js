f3ef319f0ee4c7f85ab43e75a6a6f2d3372f2e21
Grace-Bilali <grace.bilali@gmail.com>
2020-01-20 17:57:44 +0100

    single-event page footer: left part done

--- src/components/Event/EventFooter.js
+++ src/components/Event/EventFooter.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import ButtonBase from '@material-ui/core/ButtonBase';
 
 // == Import : local
 import './event.scss';
 
 // == Import : sous-composants
-// import MapLink from './MapLink';
-// import EventAddress from './EventAddress';
+import MapLink from './MapLink';
+import EventAddress from './EventAddress';
 
 // == Style du composant
 const useStyles = makeStyles((theme) => ({
@@ -16,17 +17,21 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   event_footer: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(1),
   },
   image: {
-    maxWidth: 400,
-    maxHeight: 400,
+    maxWidth: 300,
+    maxHeight: 300,
   },
   img: {
     margin: 'auto',
     display: 'block',
     maxWidth: '100%',
     maxHeight: '100%',
+  },
+  address: {
+    maxWidth: 300,
+    borderRight: 'dashed #078171',
   },
 }));
 
@@ -39,12 +44,37 @@ const EventFooter = () => {
       justify="space-evenly"
       className={classes.event_footer}
     >
-      <Grid item xs={12} md={4}>
-          Gauche
+      {/* Partie de gauche avec la map, l'adresse et le lien vers Maps */}
+      <Grid container item xs={12} md={4}>
+        <Grid item>
+          <ButtonBase className={classes.image}>
+            <img
+              className={classes.img}
+                // Sera dynamisé ?
+              alt="carte plage d'armanville"
+              src="https://www.plages.tv/gallery/cms/images/carte-plage-armaville-pirou.jpg"
+            />
+          </ButtonBase>
+        </Grid>
+        <Grid
+          container
+          item
+          direction="column"
+          className={classes.address}
+        >
+          <Grid item>
+            <MapLink />
+          </Grid>
+          <Grid item>
+            <EventAddress />
+          </Grid>
+        </Grid>
       </Grid>
+      {/* Partie du milieu avec infos sur l'event: participants, pénibilité, durée etc.. */}
       <Grid item xs={12} md={4}>
           Milieu
       </Grid>
+      {/* Partie de droite avec infos de contact de l'organisateur, date, score */}
       <Grid item xs={12} md={4}>
           Droite
       </Grid>
