9dd4cf062fe50560630c9517df6f176bbefdb146
Grace-Bilali <grace.bilali@gmail.com>
2020-01-21 14:23:40 +0100

    addition of the subcomponent Banner in the page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -4,9 +4,13 @@ import React from 'react';
 // == Import : local
 import './login.scss';
 
+// == Import : sous-composants
+import Banner from 'src/components/Banner';
+
 // == Composant
 const Login = () => (
   <div id="login">
+    <Banner />
     Login
   </div>
 );
