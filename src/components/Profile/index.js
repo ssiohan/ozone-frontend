8f14d407baf28b145864a87fce8434d71c0d1e85
Amar-Bouabbache <amarbouabbachepro@gmail.com>
2020-01-24 10:23:10 +0100

    mise en forme problème agencement left wip

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -7,8 +7,10 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import { MdSettings } from "react-icons/md";
 
+
 // == Import : local
 import './profile.scss';
+import Card from './Card';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,14 +18,16 @@ const useStyles = makeStyles(theme => ({
 
   },
   large: {
-    width: theme.spacing(15),
-    height: theme.spacing(15),
+    width: theme.spacing(20),
+    height: theme.spacing(20),
   },
   left: {
-    border: '1px solid black',
+    //border: '1px solid black',
   },
   link: {
-    border: '1px solid black',
+    //border: '1px solid black',
+    'margin-top': '0.5em',
+    'padding-left': '1em',
   },
   right: {
     border: '1px solid black',
@@ -31,6 +35,22 @@ const useStyles = makeStyles(theme => ({
   setting: {
     'margin-left': '.2em',
     'margin-top': '.1em',
+  },
+  count: {
+    'padding-top': '5em',
+  },
+  description: {
+    'margin-top': '3em',
+  },
+  descriptionTitle: {
+    'margin-top': '3em',
+    'margin-bot': '3em',
+  },
+  avatar: {
+    'margin-top': '4em',
+  },
+  cards: {
+    'margin-top': '4em',
   },
 }));
 
@@ -45,20 +65,21 @@ export default function ImageAvatars() {
           <Avatar alt="Mme Michu" src="http://www.web-soluces.net/webmaster/avatar/GetAvataaarsCom-Femme.png" className={classes.large} />
         </Grid>
         <Grid container direction="row" className={classes.link}>
-          <Typography variant="body1">modifier profil </Typography>
+          <Typography variant="body1" className={classes.changeProfil}>Modifier profil</Typography>
           <MdSettings className={classes.setting}/>
         </Grid>
-        <Grid>
-        <Typography align ='left' variant="subtitle1">description </Typography>
-        <Typography variant="body1">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</Typography>
+        <Grid align ='left'>
+        <Typography variant="subtitle1" className={classes.descriptionTitle}>Description</Typography>
+        <Typography variant="body1" className={classes.description}>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</Typography>
         </Grid>
       </Grid>
       <Grid container item xs={12} sm={3} className={classes.right}>
-        <Grid>
-        <Typography variant="subtitle1">compteur de points </Typography>
+        <Grid container justify="center">
+        <Typography className={classes.count} variant="subtitle1">Points : 150</Typography>
         </Grid>
-        <Grid>
-          <img src="src/assets/img1.jpg" alt=""/>
+        <Grid className={classes.cards}>
+          <Card />
+          <Card />
         </Grid>
       </Grid>
     </Grid>
