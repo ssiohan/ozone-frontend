fe512a8891e66d9e6b0e0bc17a3eb4501527bdbb
Grace-Bilali <grace.bilali@gmail.com>
2020-01-25 07:43:01 +0100

    harmonizing the branch with master to be able to merge

---
+++ src/store/actions.js
@@ -0,0 +1,17 @@
+/* eslint-disable linebreak-style */
+// == action types
+// export const CHANGE_INPUT_VALUE = 'CHANGE_INPUT_VALUE';
+// export const DO_LOGIN = 'DO_LOGIN';
+// export const AFTER_SEARCH = 'AFTER_SEARCH';
+
+
+// // == action creators
+// // Seule utilité : simplifier la création d'action
+// export const changeInput = (value) => ({
+//   type: CHANGE_INPUT_VALUE,
+//   value,
+// });
+// export const afterSearch = (search) => ({
+//   type: AFTER_SEARCH,
+//   search,
+// });
