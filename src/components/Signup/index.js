1c799654148fde1b7b388d8119b19eab6736f6c6
Grace-Bilali <grace.bilali@gmail.com>
2020-01-25 08:52:09 +0100

    code cleaning; removal of useless imports

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -4,8 +4,6 @@ import Grid from '@material-ui/core/Grid';
 import { FaRegUserCircle } from 'react-icons/fa';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import PropTypes from 'prop-types';
 
 
