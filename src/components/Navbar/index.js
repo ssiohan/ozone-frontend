e7fd3848448b86b4e11e4d0dd3f795c37c77ef60
Baptiste-Caral <baptiste.caral@gmail.com>
2020-01-27 17:51:17 +0100

    sidebar for tablets

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,11 +7,11 @@ import Grid from '@material-ui/core/Grid';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
 import Link from '@material-ui/core/Link';
-import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import { GiEarthAmerica } from 'react-icons/gi';
+import Drawer from './Drawer';
 
 // == Import : local
 import './navbar.scss';
@@ -19,7 +19,7 @@ import './navbar.scss';
 const lightColor = 'rgba(255, 255, 255, 0.8)';
 
 
-const styles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
 
   appBar: {
     background: '-webkit-gradient(linear, right top, left bottom, from(rgba(146,100,231,1)),to(rgba(54,204,212,1)))',
@@ -65,7 +65,6 @@ const styles = (theme) => ({
   button: {
     borderColor: lightColor,
     'margin-top': '.5em',
-    // marginTop: '10px',
   },
   ozonelogo: {
     'margin-top': '9px',
@@ -78,120 +77,114 @@ const styles = (theme) => ({
   ozoneimg: {
     'margin-top': '9px',
   },
-});
+}));
 
-function Header(props, logged) {
-  const { classes, onDrawerToggle } = props;
-  console.log(logged);
+const Header = ({ logged }) => {
 
+  // logged: value in the state: if logged is false "s'inscrire" and "se connecter" buttons appears
+  // if logged is true and <Avatar /> appear
+
+  // const onDrawerToggle = props;
+
+  const classes = useStyles();
   return (
-    <React.Fragment className={classes.colornav}>
-      <AppBar className={classes.appBar} position="sticky" elevation={0}>
-        <Toolbar>
-          <Grid container spacing={1}>
-            <Hidden mdUp>
+
+    <AppBar className={classes.appBar} position="sticky" elevation={0}>
+      <Toolbar>
+        <Grid container spacing={1}>
+          <Hidden mdUp>
+            <Grid item>
+              <Drawer className={classes.appBar} />
+            </Grid>
+          </Hidden>
+          <Hidden mdUp>
+            <Grid className={classes.ozonelogo} item xs>
+              <Typography align="left" color="inherit" variant="h5" component="h1"> </Typography>
+            </Grid>
+          </Hidden>
+          <Hidden only={['xs', 'sm']}>
+            <Grid className={classes.ozonelogo} item xs>
+              {/* <img
+                className={classes.ozoneimg}
+                src="src/assets/img/logo_sans_fond.png"
+                alt=""
+              /> */}
+              <Typography align="left" color="inherit" variant="h4" component="h1">
+                <Link underline="none" className={classes.linkearth} href="/">
+                  <GiEarthAmerica className={classes.ozoneimg} />
+                </Link>
+                <Link underline="none" className={classes.ozonetext} href="/">
+                  zone
+                </Link>
+              </Typography>
+            </Grid>
+          </Hidden>
+          <Hidden only={['xs', 'sm']}>
+            <Toolbar>
               <Grid item>
-                <IconButton
-                  color="inherit"
-                  aria-label="open drawer"
-                  onClick={onDrawerToggle}
-                  className={classes.menuButton}
-                >
-                  <MenuIcon />
-                </IconButton>
+                <Link underline="none" className={classes.link} href="/">
+                  Accueil
+                </Link>
               </Grid>
-            </Hidden>
-            <Hidden mdUp>
-              <Grid className={classes.ozonelogo} item xs>
-                <Typography align="left" color="inherit" variant="h5" component="h1"> </Typography>
+              <Grid item>
+                <Link underline="none" className={classes.link} href="/event">
+                  Les events
+                </Link>
               </Grid>
-            </Hidden>
-            <Hidden only={['xs', 'sm']}>
-              <Grid className={classes.ozonelogo} container xs>
-                {/* <img
-                  className={classes.ozoneimg}
-                  src="src/assets/img/logo_sans_fond.png"
-                  alt=""
-                /> */}
-                <Typography align="left" color="inherit" variant="h4" component="h1">
-                  <Link underline="none" className={classes.linkearth} href="/">
-                    <GiEarthAmerica className={classes.ozoneimg} />
-                  </Link>
-                  <Link underline="none" className={classes.ozonetext} href="/">
-                    zone
-                  </Link>
-                </Typography>
+              <Grid item>
+                <Link underline="none" className={classes.link} href="/create-event">
+                  Ajouter un event
+                </Link>
               </Grid>
-            </Hidden>
-            <Hidden only={['xs', 'sm']}>
-              <Toolbar>
-                <Grid wrap="nowrap" item>
-                  <Link underline="none" className={classes.link} href="/">
-                    Accueil
+              <Grid item>
+                <Link underline="none" className={classes.link} href="/about">
+                  À propos
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link underline="none" className={classes.link} href="/sponsors">
+                  Partenaires
+                </Link>
+              </Grid>
+              {!logged && (
+              // eslint-disable-next-line react/jsx-wrap-multilines
+              <Grid item>
+                <Button className={classes.button} color="inherit" size="small">
+                  <Link underline="none" className={classes.linkButton} href="/login">
+                  Se connecter
                   </Link>
-                </Grid>
-                <Grid wrap="nowrap" item>
-                  <Link underline="none" className={classes.link} href="/event">
-                    Les events
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link underline="none" className={classes.link} href="/create-event">
-                    Ajouter un event
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link underline="none" className={classes.link} href="/about">
-                    À propos
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link underline="none" className={classes.link} href="/sponsors">
-                    Partenaires
-                  </Link>
-                </Grid>
-                {logged && (
+                </Button>
+              </Grid>)}
+              {!logged && (
                 // eslint-disable-next-line react/jsx-wrap-multilines
                 <Grid item>
-                  <Button className={classes.button} variant="" color="inherit" size="small">
-                    <Link underline="none" className={classes.linkButton} href="/login">
-                    Se connecter
+                  <Button className={classes.button} variant="outlined" color="inherit" size="small">
+                    <Link
+                      underline="none"
+                      className={classes.linkButton}
+                      href="/signup"
+                    >
+                    S'inscrire
                     </Link>
                   </Button>
                 </Grid>)}
-                {logged && (
-                  // eslint-disable-next-line react/jsx-wrap-multilines
-                  <Grid item>
-                    <Button className={classes.button} variant="outlined" color="inherit" size="small">
-                      <Link
-                        underline="none"
-                        className={classes.linkButton}
-                        href="/signup"
-                      >
-                      S'inscrire
-                      </Link>
-                    </Button>
-                  </Grid>)}
-                {!logged && (
-                  // eslint-disable-next-line react/jsx-wrap-multilines
-                  <Grid item>
-                    <IconButton color="inherit" className={classes.iconButtonAvatar}>
-                      <Avatar src="/static/images/avatar/1.jpg" alt="My Avatar" />
-                    </IconButton>
-                  </Grid>)}
-              </Toolbar>
-            </Hidden>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-    </React.Fragment>
-
+              {logged && (
+                // eslint-disable-next-line react/jsx-wrap-multilines
+                <Grid item>
+                  <IconButton color="inherit" className={classes.iconButtonAvatar}>
+                    <Avatar src="/static/images/avatar/1.jpg" alt="My Avatar" />
+                  </IconButton>
+                </Grid>)}
+            </Toolbar>
+          </Hidden>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
-}
-
+};
 Header.propTypes = {
-  classes: PropTypes.object.isRequired,
-  onDrawerToggle: PropTypes.func.isRequired,
+
+  logged: PropTypes.bool.isRequired,
 };
 
-export default withStyles(styles)(Header);
+export default Header;
