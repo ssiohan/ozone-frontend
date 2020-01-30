3f428981a5af1468926d78e0af112d670f733e1d
Grace-Bilali <grace.bilali@gmail.com>
2020-01-30 17:28:47 +0100

    form complete but not responsive yet

--- src/components/CreateEvent/CreateForm.js
+++ src/components/CreateEvent/CreateForm.js
@@ -67,180 +67,197 @@ const CreateForm = () => {
       <Grid
         container
         direction="row"
+        justify="space-around"
       >
         {/* Partie de gauche */}
-        <Grid
-          item
-          container
-          direction="column"
-          justify="center"
-        >
-          <Grid item>
-            <TextField
-              id="event-title"
-              label="Titre de l'événement"
-              variant="outlined"
-              type="text"
-              name="eventTitle"
-              className={classes.fields}
-            />
-          </Grid>
-          <Grid item>
-            <TextField
-              select
-              id="typeEvent"
-              variant="outlined"
-              label="Type d'événement"
-              name="typeEvent"
-              defaultValue="atelier"
-              className={classes.fields}
-              SelectProps={{
-                native: true,
-              }}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            >
-              <option value="atelier">Atelier</option>
-              <option value="acte citoyen">Acte citoyen</option>
-              <option value="réunion d'information">Réunion d'information</option>
-            </TextField>
-          </Grid>
-          <Grid item>
-            <InputLabel>Description de l'événement</InputLabel>
-            <TextareaAutosize
-              id="event-description"
-              label="Description de l'événement"
-              aria-label="Description de l'événement"
-              placeholder="Description de l'événement"
-              variant="outlined"
-              rowsMin={20}
-              name="eventDescription"
-              className={classes.description}
-            />
-          </Grid>
-
-        </Grid>
-        {/* Début de la partie de droite */}
-        <Grid
-          item
-          container
-          direction="column"
-          justify="center"
-        >
+        <Grid item>
           <Grid
-            container
             item
-            alignContent="center"
+            container
+            direction="column"
             justify="center"
           >
             <Grid item>
               <TextField
-                id="event-date"
+                id="event-title"
+                label="Titre de l'événement"
                 variant="outlined"
-                label="Date de l'événement"
-                type="date"
-                name="eventDate"
-                className={classes.dateAndTime}
-                InputLabelProps={{
-                  shrink: true,
-                }}
+                type="text"
+                name="eventTitle"
+                className={classes.fields}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                select
+                id="typeEvent"
+                variant="outlined"
+                label="Type d'événement"
+                name="typeEvent"
+                defaultValue="atelier"
+                className={classes.fields}
                 SelectProps={{
                   native: true,
                 }}
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                id="event-duration"
-                variant="outlined"
-                label="Durée de l'événement"
-                type="time"
-                defaultValue="00:00"
-                name="eventDuration"
-                className={classes.dateAndTime}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-            </Grid>
-          </Grid>
-          <Grid
-            container
-            item
-            alignContent="center"
-            justify="center"
-          >
-            <Grid item>
-              <TextField
-                select
-                id="min-participants"
-                variant="outlined"
-                label="Nombre minimum de participants"
-                name="min-participants"
-                defaultValue={1}
-                className={classes.dateAndTime}
                 InputLabelProps={{
                   shrink: true,
                 }}
               >
-                {optionsList()}
+                <option value="atelier">Atelier</option>
+                <option value="acte citoyen">Acte citoyen</option>
+                <option value="réunion d'information">Réunion d'information</option>
               </TextField>
             </Grid>
             <Grid item>
-              <TextField
-                select
-                id="max-participants"
+              <InputLabel>Description de l'événement</InputLabel>
+              <TextareaAutosize
+                id="event-description"
+                label="Description de l'événement"
+                aria-label="Description de l'événement"
+                placeholder="Description de l'événement"
                 variant="outlined"
-                label="Nombre maximum de participants"
-                name="max-participants"
-                defaultValue={999}
-                className={classes.dateAndTime}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              >
-                {optionsList()}
-              </TextField>
+                rowsMin={12}
+                name="eventDescription"
+                className={classes.description}
+              />
             </Grid>
-          </Grid>
-          <Grid item>
-            <TextField
-              type="file"
-              id="event-picture"
-              variant="outlined"
-              label="Ajouter une photo de l'événement"
-              name="event-picture"
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-          </Grid>
-          <Grid item>
-            <Typography>
-              <GoGraph /> Penibilité
-              <Rating name="painfulness" size="small" />
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography>
-              <MdAccessTime /> Durée
-              <Rating name="duration" size="small" />
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography>
-              <FaPeopleCarry /> Impact société
-              <Rating name="impactSocietal" size="small" />
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography>
-              <GiRecycle /> Impact environnement
-              <Rating name="impactEnvironnemental" size="small" />
-            </Typography>
+
           </Grid>
         </Grid>
+        {/* Partie de droite */}
+        <Grid item>
+          <Grid
+            item
+            container
+            direction="column"
+            justify="center"
+          >
+            <Grid
+              container
+              item
+              alignContent="center"
+              justify="center"
+            >
+              <Grid item>
+                <TextField
+                  id="event-date"
+                  variant="outlined"
+                  label="Date de l'événement"
+                  type="date"
+                  name="eventDate"
+                  className={classes.dateAndTime}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  SelectProps={{
+                    native: true,
+                  }}
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="event-duration"
+                  variant="outlined"
+                  label="Durée de l'événement"
+                  type="time"
+                  defaultValue="00:00"
+                  name="eventDuration"
+                  className={classes.dateAndTime}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              item
+              alignContent="center"
+              justify="center"
+            >
+              <Grid item>
+                <TextField
+                  select
+                  id="min-participants"
+                  variant="outlined"
+                  label="Nombre minimum de participants"
+                  name="min-participants"
+                  defaultValue={1}
+                  className={classes.dateAndTime}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                >
+                  {optionsList()}
+                </TextField>
+              </Grid>
+              <Grid item>
+                <TextField
+                  select
+                  id="max-participants"
+                  variant="outlined"
+                  label="Nombre maximum de participants"
+                  name="max-participants"
+                  defaultValue={999}
+                  className={classes.dateAndTime}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                >
+                  {optionsList()}
+                </TextField>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <TextField
+                type="file"
+                id="event-picture"
+                variant="outlined"
+                label="Ajouter une photo de l'événement"
+                name="event-picture"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <Typography>
+                <GoGraph /> Penibilité
+                <Rating name="painfulness" size="small" />
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography>
+                <MdAccessTime /> Durée
+                <Rating name="duration" size="small" />
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography>
+                <FaPeopleCarry /> Impact société
+                <Rating name="impactSocietal" size="small" />
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography>
+                <GiRecycle /> Impact environnement
+                <Rating name="impactEnvironnemental" size="small" />
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      {/* En-dessous, le bouton */}
+      <Grid item xs={12}>
+        <Button
+          variant="contained"
+          type="submit"
+          color="default"
+          size="large"
+          className={classes.button}
+        >
+        Publication
+        </Button>
       </Grid>
 
     </form>
