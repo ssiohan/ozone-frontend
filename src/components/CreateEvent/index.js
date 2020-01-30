99ead5d1b27e2dcba0f57ffece4a12d0f61a26ed
Grace-Bilali <grace.bilali@gmail.com>
2020-01-30 08:02:45 +0100

    first intentions

--- src/components/CreateEvent/index.js
+++ src/components/CreateEvent/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 // == Import : npm
 import React from 'react';
 
@@ -7,7 +8,15 @@ import './createEvent.scss';
 // == Composant
 const CreateEvent = () => (
   <div id="createEvent">
-    CreateEvent
+    Banner
+    Formulaire de création:
+     - Titre
+     - Date & durée
+     - Description
+     - Participants
+     - Image
+     - Rating: pénibilité, durée, impact société, impact environnement.
+    Preview en live
   </div>
 );
 
