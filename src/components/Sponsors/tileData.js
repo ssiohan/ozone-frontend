fd5b0e2e32007814b1a73d8f331fab5809e7f1e7
Grace-Bilali <grace.bilali@gmail.com>
2020-02-05 09:28:31 +0100

    partner page : wip

---
+++ src/components/Sponsors/tileData.js
@@ -0,0 +1,27 @@
+// Import des images
+import savon from 'src/assets/img/savon.jpg';
+import pailles from 'src/assets/img/pailles.jpg';
+import Encens from 'src/assets/img/Encens.jpg';
+// import giftcard from 'src/assets/img/giftcard.jpg';
+
+// == Sources de données
+const tileData = [
+  {
+    img: savon,
+    title: 'Savon de Marseille Bio',
+    points: '20',
+  },
+  {
+    img: pailles,
+    title: 'Set de 4 pailles en inox',
+    points: '35',
+  },
+  {
+    img: Encens,
+    title: 'Boîte de 16 cônes ayurvédiques Vata',
+    points: '15',
+  },
+];
+
+// == Export
+export default tileData;
